78a0fe97770ba251dddbc5878b7e34b45075d38f
defa hangga <defa.hagga@gmail.com>
2020-07-30 04:32:52 +0700

    selesai tukar-besar-kecil.php

--- tukar-besar-kecil.php
+++ tukar-besar-kecil.php
@@ -1,28 +1,25 @@
 <?php
 function tukar_besar_kecil($string){
-    $abjad = "abcdefghijklmnopqrstuvwx";
-    $uppAbjad = strtoupper($abjad);
-    $output = "";
-    $posAbjad = 0;
-    for ($i = 0; $i < strlen($string)-1; $i++){
-        $posAbjad .= strrpos($abjad, strtolower($string[$i]));
-        if ( strtolower($string[$posAbjad]) == $abjad[$posAbjad]){
-            $output .= $uppAbjad[$posAbjad];
+    $stemp = "";
+    for($i = 0; $i < strlen($string); $i++){
+        $str = $string[$i];
+        if(ctype_alpha($str) && $str === strtolower($str)){
+            $stemp .= strtoupper($str);
+        } else if (ctype_alpha($str) && $str === strtoupper($str)){
+            $stemp .= strtolower($str);
         } else {
-            $output .= $abjad[$posAbjad];
+            $stemp .= $string[$i];
         }
-
-
     }
-    return $output;
+    return $stemp;
 
 }
  
 // TEST CASES
-echo tukar_besar_kecil('Hello World'); // "hELLO wORLD"
-// echo tukar_besar_kecil('I aM aLAY'); // "i Am Alay"
-// echo tukar_besar_kecil('My Name is Bond!!'); // "mY nAME IS bOND!!"
-// echo tukar_besar_kecil('IT sHOULD bE me'); // "it Should Be ME"
-// echo tukar_besar_kecil('001-A-3-5TrdYW'); // "001-a-3-5tRDyw"
+echo "<br>" . tukar_besar_kecil('Hello World'); // "hELLO wORLD"
+echo "<br>" . tukar_besar_kecil('I aM aLAY'); // "i Am Alay"
+echo "<br>" . tukar_besar_kecil('My Name is Bond!!'); // "mY nAME IS bOND!!"
+echo "<br>" . tukar_besar_kecil('IT sHOULD bE me'); // "it Should Be ME"
+echo "<br>" . tukar_besar_kecil('001-A-3-5TrdYW'); // "001-a-3-5tRDyw"
 
 ?>
