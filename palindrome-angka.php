b8c29c0889e1bdbcb39b3c8c63470c8fea0db6de
defa hangga <defa.hagga@gmail.com>
2020-07-29 17:21:55 -0700

    menambahkan file papan-catur.php

--- palindrome-angka.php
+++ palindrome-angka.php
@@ -5,10 +5,10 @@ function palindrome_angka($angka) {
 }
 
 // TEST CASES
-echo palindrome_angka(8) // 9
-echo palindrome_angka(10); // 11
-echo palindrome_angka(117); // 121
-echo palindrome_angka(175); // 181
-echo palindrome_angka(1000); // 1001
+// echo palindrome_angka(8) // 9
+// echo palindrome_angka(10); // 11
+// echo palindrome_angka(117); // 121
+// echo palindrome_angka(175); // 181
+// echo palindrome_angka(1000); // 1001
 
 ?>
